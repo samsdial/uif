a569200be2cf5972209ece4c0960303d2f740e02
SergioAndresMartinez <sergio.martinez@fracttal.com>
2023-10-27 09:13:14 -0500

    add cambios iconos ajustes

--- src/components/Icons/ListTableIcons.stories.tsx
+++ src/components/Icons/ListTableIcons.stories.tsx
@@ -6,35 +6,43 @@ const variantNames = Object.keys(ICON_VARIANTS) as Array<
   keyof typeof ICON_VARIANTS
 >;
 
+const padding = "6px";
+
 export const ListTableIcons: React.FC = () => {
   return (
     <table>
       <thead>
         <tr>
-          <th>Variant Name</th>
-          <th>Icon</th>
+          <th>VariantName</th>
+          <th>small</th>
+          <th>medium</th>
+          <th>large</th>
+          <th>xlarge</th>
+          <th> xxlarge / Blue</th>
         </tr>
       </thead>
       <tbody>
         {variantNames.map((variantName) => (
           <tr key={variantName}>
-            <td>{variantName}</td>
             <td>
+              <h3>{variantName}</h3>
+            </td>
+            <td style={{ padding: padding }}>
               <Icon variantName={variantName} size="small" color="#000000" />
             </td>
-            <td>
+            <td style={{ padding: padding }}>
               <Icon variantName={variantName} size="medium" color="#000000" />
             </td>
-            <td>
+            <td style={{ padding: padding }}>
               <Icon variantName={variantName} size="large" color="#000000" />
             </td>
-            <td>
+            <td style={{ padding: padding }}>
               <Icon variantName={variantName} size="xlarge" color="#000000" />
             </td>
-            <td>
-              <Icon variantName={variantName} size="xxlarge" color="#000000" />
+            <td style={{ padding: padding }}>
+              <Icon variantName={variantName} size="xxlarge" color="#4687f1" />
             </td>
-            <td style={{ backgroundColor: "#4687f1" }}>
+            <td style={{ padding: padding, backgroundColor: "#4687f1" }}>
               <Icon variantName={variantName} size="xxlarge" color="#fff" />
             </td>
           </tr>
